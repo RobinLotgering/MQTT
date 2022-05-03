b762f086ee224623e0a4b18e6c452b0106dcd2a3
RobinLotgering <robinolotje@gmail.com>
2022-05-03 15:39:18 +0200

    start modbus trial

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pymodbus.payload import BinaryPayloadDecoder
 Defaults.Timeout = 25
 Defaults.Retries = 5
 client = ModbusClient('10.10.0.233', port='502')
-result = client.read_input_registers(239, count=2, unit=239)
+result = client.read_holding_registers(239, count=2, unit=239)
 decoder = BinaryPayloadDecoder.fromRegisters(result.registers, byteorder=Endian.Big)
 voltage = decoder.decode_16bit_uint()
 print("Battery voltage: {0:.2f}V".format(voltage / 10.0))
